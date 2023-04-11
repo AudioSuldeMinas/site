d64cdf3f094952b65eb002bb0ac81f7ca709d7a5
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-04-11 10:50:46 -0300

    email

--- components/Formulario.jsx
+++ components/Formulario.jsx
@@ -54,16 +54,10 @@ export default function Formulario() {
     }
 
     const onReCAPTCHAChange = (captchaCode) => {
-        // If the reCAPTCHA code is null or undefined indicating that
-        // the reCAPTCHA was expired then return early
+       
         if(!captchaCode) {
           return;
-        }
-        // Else reCAPTCHA was executed successfully so proceed with the 
-        // alert
-        alert(`Hey, ${email}`);
-        // Reset the reCAPTCHA so that it can be executed again if user 
-        // submits another email.
+        } 
         recaptchaRef.current.reset();
       }
 
